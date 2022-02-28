3d61efa73394ef8c6e2802774d06349e3e284d23
jspearing <jody@jdspearing.com>
2022-02-28 12:25:33 -0500

    updated

---
+++ app/app.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Text } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Screen from "./app/components/Screen";
+
+const Tweets = () => (
+    <Screen>
+        <Text>Tweet</Text>
+    </Screen>
+);
+
+const TweetDetails = () => (
+    <Screen>
+        <Text>TweetDetails</Text>
+    </Screen>
+);
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => (
+    <Stack.Navigator>
+        <Stack.Screen name="Tweets" component={Tweets} />
+        <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    </Stack.Navigator>
+)
+export default function App() {
+    return (
+        <NavigationContainer>
+            <StackNavigator />
+        </NavigationContainer>
+    );
+}

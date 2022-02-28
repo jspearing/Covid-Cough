b308e2fa549d5ea44e78e0acf7cce0087a4e6b97
jspearing <jody@jdspearing.com>
2022-02-28 13:17:13 -0500

    updated

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { Text } from 'react-native';
+import { Text, Button } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+
 import Screen from "./app/components/Screen";
-import { Button } from "react-native-web";
-import navigationTheme from "./app/navigation/navigationTheme";
+
+//import navigationTheme from "./app/navigation/navigationTheme";
 
 const Tweets = ({ navigation }) => (
     <Screen>
         <Text>Tweets</Text>
         <Button
-            title = "View Tweet"
-            onPress={() => navigation.navigate("TweetDetails")} 
+            title="View Tweet"
+            //onPress={() => navigation.navigate("TweetDetails")} 
         />
     </Screen>
 );

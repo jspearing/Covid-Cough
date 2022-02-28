5f66a66042d6d098a2cde88c9c3929a35286b9aa
jspearing <jody@jdspearing.com>
2022-02-28 10:54:55 -0500

    updated

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
-import navigationTheme from "./app/navigation/navigationTheme";
-import AppNavigator from "./app/navigation/AppNavigator";
+import navigationTheme from "../navigation/navigationTheme";
+import AppNavigator from "../navigation/AppNavigator";
 
 export default function App() {
   return (

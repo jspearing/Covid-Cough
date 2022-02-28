5216a6e49dc30d33b0ff73b771c504e2170051f3
jspearing <jody@jdspearing.com>
2022-02-28 12:29:18 -0500

    updated

--- app.js
+++ app.js
@@ -1,33 +1,13 @@
 import React from "react";
-import { Text } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import Screen from "./app/components/Screen";
+import { NavigationContainer } from "@react-navigation/native";
 
-const Tweets = () => (
-    <Screen>
-        <Text>Tweet</Text>
-    </Screen>
-);
+import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
 
-const TweetDetails = () => (
-    <Screen>
-        <Text>TweetDetails</Text>
-    </Screen>
-);
-
-const Stack = createStackNavigator();
-
-const StackNavigator = () => (
-    <Stack.Navigator>
-        <Stack.Screen name="Tweets" component={Tweets} />
-        <Stack.Screen name="TweetDetails" component={TweetDetails} />
-    </Stack.Navigator>
-)
 export default function App() {
-    return (
-        <NavigationContainer>
-            <StackNavigator />
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
